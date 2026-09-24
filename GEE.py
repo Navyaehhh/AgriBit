dfba54ff91b6894ecd181f05bf01cb410274e6fe
Navyeahhh <navyachaturvedi238@gmail.com>
2026-09-25 00:33:18 +0530

    GEE Complete Integration

--- GEE.py
+++
@@ -1,6 +0,0 @@
-import ee
-ee.Initialize(project="agribit-499813")   # Replace with your GCP project ID if needed
-print("Connected to Earth Engine!")
-startDate = ee.Date('2024-06-01')
-endDate   = ee.Date('2025-10-01')
-stepDays  = 15
